7f47282cc3b035ee5f34116d3bf5bdfa7ac85ccf
Kitel1 <132992502+Kitel1@users.noreply.github.com>
2023-05-10 03:57:56 +0900

    Add files via upload

---
+++ common/js/javascript.js
@@ -0,0 +1,6 @@
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction( ) {
+    
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
